8bce2c65d8612768f95bd4529216c52ebe046290
Joseph C Wang <joequant@gmail.com>
2020-08-28 23:47:01 +0800

    revert proxy at end

--- build-spack.sh
+++ build-spack.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 source /usr/sbin/proxy.sh
 spack install -v key4hep-stack
+if [ -f /home/user/.spack/linux/compilers.yaml.noproxy ] ; then
+pushd /home/user/.spack/linux
+cp -f compilers.yaml.noproxy compilers.yaml
+popd
+fi

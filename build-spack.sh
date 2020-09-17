23da76e31ef31211cc277e83b0ab2ff72cebeaa6
Joseph C Wang <joequant@gmail.com>
2020-09-18 01:02:24 +0800

    don't use push mode
    Push mode causes too many build issues.

--- build-spack.sh
+++ build-spack.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+set -e
 source /usr/sbin/proxy.sh
-spack install -j12 -v key4hep-stack
 export DISTCC_HOSTS='172.17.0.1'
-spack install -j12 -v bison
-export DISTCC_HOSTS='172.17.0.1,cpp,lzo'
 spack install -j12 -v key4hep-stack
 if [ -f /home/user/.spack/linux/compilers.yaml.noproxy ] ; then
 pushd /home/user/.spack/linux

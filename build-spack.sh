024f4e8f6ada08adcc3867d7882b8bc35caf39ba
Joseph C Wang <joequant@gmail.com>
2020-09-19 18:25:20 +0800

    add lzo compression

--- build-spack.sh
+++ build-spack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 source /usr/sbin/proxy.sh
-export DISTCC_HOSTS='172.17.0.1'
+export DISTCC_HOSTS='172.17.0.1,lzo'
 spack install -j16 -v key4hep-stack
 if [ -f /home/user/.spack/linux/compilers.yaml.noproxy ] ; then
 pushd /home/user/.spack/linux

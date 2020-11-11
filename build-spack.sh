34ffe30565f9288d5716d9819c72bc63d3b10eb5
Joseph C Wang <joequant@gmail.com>
2020-11-11 11:43:18 +0800

    change build spack to use args

--- build-spack.sh
+++ build-spack.sh
@@ -4,7 +4,10 @@ source /usr/sbin/proxy.sh
 # remove locks
 rm -f /opt/spack/.spack-db/prefix_lock
 export DISTCC_HOSTS='172.17.0.1,lzo'
-spack install -j16 -v $1
+for pkg in "$@"
+do
+    spack install -j16 -v $pkg
+done
 if [ -f /home/user/.spack/linux/compilers.yaml.noproxy ] ; then
 pushd /home/user/.spack/linux
 cp -f compilers.yaml.noproxy compilers.yaml

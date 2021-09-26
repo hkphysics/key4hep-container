f1dccad002b1c3bce7e41761a4d11156192240b7
Joseph C Wang <joequant@gmail.com>
2021-09-26 11:46:34 +0800

    remove spack deprecated flag

--- build-spack.sh
+++ build-spack.sh
@@ -6,7 +6,7 @@ rm -f /opt/spack/.spack-db/prefix_lock
 export DISTCC_HOSTS='172.17.0.1,lzo'
 for pkg in "$@"
 do
-    spack install -j16  --deprecated -v $pkg
+    spack install -j16 -v $pkg
 done
 if [ -f /home/user/.spack/linux/compilers.yaml.noproxy ] ; then
 pushd /home/user/.spack/linux

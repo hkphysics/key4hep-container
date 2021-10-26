8c12779e87ec0db5f2faa6ecff072e6fbc279bd6
Joseph C Wang <joequant@gmail.com>
2021-10-26 18:30:43 +0800

    add clang

---
+++ build-spack-clang.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+. /usr/sbin/proxy.sh
+# remove locks
+set -e
+rm -f /opt/spack/.spack-db/prefix_lock
+export DISTCC_HOSTS='172.17.0.1,lzo'
+for pkg in "$@"
+do
+    spack install -j16 -v $pkg
+done
+pushd /home/user/.spack/linux
+cp -f compilers.yaml.clang compilers.yaml
+popd
+

616ca5d723f0db95e724eb3f79096b42b39d0bf5
Joseph C Wang <joequant@gmail.com>
2023-04-25 11:43:32 +0800

    change permission for spack

--- install.sh
+++ install.sh
@@ -108,6 +108,7 @@ buildah run $container -- chmod -R o+r /opt/spack
 buildah run $container -- find /opt/spack -type d -exec chmod 775 {} \;
 chmod 0755 $rootfsDir/usr/sbin/*.sh
 buildah run $container -- chown user:user -R /home/user/.spack
+buildah run $container -- chmod a+x /opt/spack/bin/spack
 buildah run $container -- sudo -u user /opt/spack/bin/spack repo add /opt/spack/var/spack/repos/key4hep-spack
 buildah run $container --  update-distcc-symlinks
 #bootstrap clingo

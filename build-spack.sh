1279a75221bb7dbc95c6f570ccb9f2cecac7c236
Joseph C Wang <joequant@gmail.com>
2024-12-25 08:32:24 +0800

    workaround spack updates

--- build-spack.sh
+++ build-spack.sh
@@ -31,9 +31,10 @@ git fetch origin develop
 git pull --unshallow
 
 git checkout develop .
-#git checkout develop .
-#git checkout cb3d6549c988cb914583e4d220a2d1c0b0aa6ae2^ ./lib/spack/spack
-#git checkout develop ./lib/spack/spack/util/ ./lib/spack/spack/version
+# key4hep goes into infinite loop with key4hep-stack                                                                                                                                                                                       
+# see https://github.com/spack/spack/issues/47888
+git checkout cb3d6549c988cb914583e4d220a2d1c0b0aa6ae2^ ./lib/spack/spack
+git checkout 858c7ca1a2f4e0022e93faa9f91d7215a1c41b42 ./lib/spack/spack/util/ ./lib/spack/spack/version
 
 curl https://github.com/spack/spack/compare/develop...hkphysics:spack:dev/fixes.patch | patch -p1
 popd
